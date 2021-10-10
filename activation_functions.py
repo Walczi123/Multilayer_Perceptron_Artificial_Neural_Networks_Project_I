ead5998c79d28090b6bafa6f9eae4a90150cf4cf
Walczi123 <patryk.wal@onet.pl>
2021-10-10 15:20:16 +0200

    init active funtions

---
+++ activation_functions.py
@@ -0,0 +1,8 @@
+import math
+import numpy as np
+
+def sigmoid(x):
+  return 1 / (1 + math.exp(-x))
+
+def tanh(x):
+    return np.tanh(x)
--- perceptron.py
+++ perceptron.py
@@ -1,16 +1,30 @@
-class Perceptron:
+from activation_functions import sigmoid
+
+
+class PerceptronNN:
     """
     Neural Network Class
     """
 
-    def __init__(self) -> None:
-        pass 
+    def __init__(self, layers:list, activation_function):
+        self.activation_function = activation_function
 
-    def backpropagation():
+    def backpropagation(self):
         pass
 
-    def feed_forward():
+    def feed_forward(self):
+        x = self.activation_function(2)
+        print(x)
+
+    def train(self):
         pass
 
-    def train():
+    def predict(self):
+        return self.feed_forward()
+
+    def test(self):
         pass
+
+if __name__ == "__main__":
+    perceptron = PerceptronNN([2, 2, 1], sigmoid)
+    perceptron.predict()
