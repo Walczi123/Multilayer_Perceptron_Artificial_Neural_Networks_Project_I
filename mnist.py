bb76244ca89a386eed375819719d57c1319ce7b6
Walczi123 <patryk.wal@onet.pl>
2021-11-05 18:23:12 +0100

    minst

--- mnist.py
+++ mnist.py
@@ -1,6 +1,7 @@
 import ssl
+import time
 from tensorflow.keras.datasets import mnist
-from tensorflow import keras
+# from tensorflow import keras
 from matplotlib import pyplot
 from common.functions import function_type
 from common.problem_type import problem_type
@@ -23,13 +24,19 @@ else:
 # test_y - list of labels of MN IST testing images
 (train_X, train_y), (test_X, test_y) = mnist.load_data()
 
+TAKE_PART = 1
+
 train_dataset = []
-# for idx in range(len(train_X)):
-for idx in range(300):
+# r = len(train_X)
+r_train = int(len(train_X)/TAKE_PART)
+for idx in range(r_train):
+# for idx in range(300):
     train_dataset.append([np.array(train_X[idx].flatten()), train_y[idx]])
 test_dataset = []
-# for idx in range(len(test_X)):
-for idx in range(10):
+# r = len(test_X)
+r_test = int(len(test_X)/TAKE_PART)
+for idx in range(r_test):
+# for idx in range(10):
     test_dataset.append([np.array(test_X[idx].flatten()), test_y[idx]])
 
 image_len = 28 * 28
@@ -38,26 +45,41 @@ PROBLEM_TYPE = problem_type.Classification
 ACTIVATION_FUNCTION = function_type.Sigmoid
 OUTPUT_FUNCTION = function_type.Softmax
 LOSS_FUNCTION = function_type.Cross_entropy
-LAYERS = [image_len, 256, 128, classes_no]
-EPOCHS = 300
-LEARINN_RATE = 5
+# LAYERS = [image_len, classes_no]
+LAYERS = [image_len, 700, 500, classes_no]
+EPOCHS = 40
+LEARINN_RATE = 0.001
 SEED = 141
-SHOW_PERCENTAGE = 1
+SHOW_PERCENTAGE = 0.01
 BIAS = True
+
+def save_to_file(rate):
+    timestr = time.strftime("%d_%m_%Y-%H_%M-%S")
+    f = open(f"MNIST_{timestr}", "w")
+    f.write(f"Rate: {rate}\n")
+    f.write(f"Layers: {LAYERS}\n")
+    f.write(f"Seed: {SEED}\n")
+    f.write(f"Learning rate: {LEARINN_RATE}\n")
+    f.write(f"Activation function: {ACTIVATION_FUNCTION.__name__}\n")
+    f.write(f"Output function: {OUTPUT_FUNCTION.__name__}\n")
+    f.write(f"Epochs: {EPOCHS}\n")
+    if TAKE_PART != 1:
+        f.write(f"Part of dataset: {TAKE_PART}\n")
+    f.close
 
 if __name__ == "__main__":
     perceptron = MLP(PROBLEM_TYPE, LAYERS, ACTIVATION_FUNCTION,
                      OUTPUT_FUNCTION, LOSS_FUNCTION, EPOCHS, LEARINN_RATE, SEED, BIAS)
 
     perceptron.train(train_dataset, SHOW_PERCENTAGE, category_shift=0)
-    _, _, predictions = perceptron.test(
+    rate, _, _ = perceptron.test(
         test_dataset, SHOW_PERCENTAGE, category_shift=0)
 
-    for i in range(30):
-        test_case = perceptron.predict(test_X[500  + i].flatten())
-        pyplot.title(label=f"Label: {test_y[500 + i]} - Predicted: {test_case}")
-        pyplot.imshow(test_X[500 + i], cmap=pyplot.get_cmap('gray'))
-        pyplot.show()
+    # for i in range(30):
+    #     test_case = perceptron.predict(test_X[500  + i].flatten())
+    #     pyplot.title(label=f"Label: {test_y[500 + i]} - Predicted: {test_case}")
+    #     pyplot.imshow(test_X[500 + i], cmap=pyplot.get_cmap('gray'))
+    #     pyplot.show()
 
     # print(train_X[0])
     # print(train_X[0].shape)
@@ -65,3 +87,8 @@ if __name__ == "__main__":
     # print(train_X[0].flatten().shape)
     # print(len(train_X))
     # print(len(train_y))
+    # print(len(test_X))
+    # print(len(test_y))
+    # print(len(train_dataset))
+    # print(len(test_dataset))
+    save_to_file(rate)
